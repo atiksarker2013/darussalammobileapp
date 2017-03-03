88ea2fd740bc813c007d04b7ef31ab51640cb0b6
Md. Atiour Islam <atiour.islam@gmail.com>
2017-03-03 11:57:22 +0600

    jjj

---
+++ darussalambd/darussalambd/Models/tbl_DarussalamMobileCart.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace darussalambd.Models
+{
+    public  class tbl_DarussalamMobileCart
+    {
+
+        public int Id { get; set; }
+        public string OrderId { get; set; }
+        public int? CUstomerId { get; set; }
+        public int? BookId { get; set; }
+        public string BookName { get; set; }
+        public decimal Price { get; set; }
+        public int? Qty { get; set; }
+        public decimal? UnitTotal { get; set; }
+        public DateTime? EntryDate { get; set; }
+        public bool? SubmitStatus { get; set; }
+        public bool? ProcessStatus { get; set; }
+        
+    }
+}
---
+++ darussalambd/darussalambd/Services/CartService.cs
@@ -0,0 +1,25 @@
+﻿using darussalambd.Models;
+using Plugin.RestClient;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace darussalambd.Services
+{
+    public class CartService
+    {
+        public async Task<List<Models.tbl_DarussalamMobileCart>> GetCartsAsync(string url)
+        {
+            RestClient<Models.tbl_DarussalamMobileCart> restClient = new Plugin.RestClient.RestClient<Models.tbl_DarussalamMobileCart>();
+            var userList = await restClient.GetAsync(url);
+            return userList;
+        }
+
+        public async Task PostCartAsync(tbl_DarussalamMobileCart _object, string url)
+        {
+
+            RestClient<Models.tbl_DarussalamMobileCart> restClient = new Plugin.RestClient.RestClient<Models.tbl_DarussalamMobileCart>();
+            var user = await restClient.PostAsync(_object, url);
+            // return item;
+        }
+    }
+}
